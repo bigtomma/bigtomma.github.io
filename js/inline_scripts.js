9fec879b44f6b97713768dd07a49c0688b8503d0
bigtomma <lollipopparklz@gmail.com>
2025-05-25 17:28:19 +0200

    Add files via upload

---
+++ js/inline_scripts.js
@@ -0,0 +1,204 @@
+document.documentElement.className = document.documentElement.className.replace( /\bno-js\b/,'js' );
+
+
+/* <![CDATA[ */
+jQuery(document).ready(function() {
+	jQuery(".92f2483bb651eed307c93e619d752c78").click(function() {
+		jQuery.post(
+			"https://itexamanswers.net/wp-admin/admin-ajax.php", {
+				"action": "quick_adsense_onpost_ad_click",
+				"quick_adsense_onpost_ad_index": jQuery(this).attr("data-index"),
+				"quick_adsense_nonce": "8a11acd4e3",
+			}, function(response) { }
+		);
+	});
+});
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var wpdm_url = {"home":"https:\/\/itexamanswers.net\/","site":"https:\/\/itexamanswers.net\/","ajax":"https:\/\/itexamanswers.net\/wp-admin\/admin-ajax.php"};
+var wpdm_js = {"spinner":"<i class=\"fas fa-sun fa-spin\"><\/i>","client_id":"b34a1a2a0c01857d16f93e4493d0425a"};
+var wpdm_strings = {"pass_var":"Password Verified!","pass_var_q":"Please click following button to start download.","start_dl":"Start Download"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var dwqa_gcv2 = {"public_key":"6LfuC78ZAAAAAFMwZMuOx4BzNZIaDOCn-vXtYUAg"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+/* Run FooBox FREE (v2.7.34) */
+var FOOBOX = window.FOOBOX = {
+	ready: true,
+	disableOthers: false,
+	o: {wordpress: { enabled: true }, hideScrollbars:false, showCount:false, captions: { dataTitle: ["captionTitle","title"], dataDesc: ["captionDesc","description"] }, rel: '', excludes:'.fbx-link,.nofoobox,.nolightbox,a[href*="pinterest.com/pin/create/button/"]', affiliate : { enabled: false }, error: "Could not load the item"},
+	selectors: [
+		".wp-caption", ".wp-block-image", "a:has(img[class*=wp-image-])", ".foobox"
+	],
+	pre: function( $ ){
+		// Custom JavaScript (Pre)
+		
+	},
+	post: function( $ ){
+		// Custom JavaScript (Post)
+		
+		// Custom Captions Code
+		
+	},
+	custom: function( $ ){
+		// Custom Extra JS
+		
+	}
+};
+/* ]]> */
+
+
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const style = document.createElement("style");
+        style.textContent = `
+            div#adngin-siderail_left-0 {
+            position: fixed;
+            display: block;
+            overflow: visible;
+            top: 40px;
+            left: 20px;
+            transition: all 500ms ease-in;
+            z-index: 999;
+            margin: 0;
+            padding: 0;
+            }
+            `;
+        document.head.appendChild(style);
+    });
+
+
+
+{
+    "@context": "http://schema.org",
+    "@type": "BlogPosting",
+    "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": "https://itexamanswers.net/itn-version-7-00-final-pt-skills-assessment-ptsa-exam-answers.html"
+    },
+    "headline": "ITN (Version 7.00) Final PT Skills Assessment (PTSA) Exam Answers",
+    "image": {
+        "@type": "ImageObject",
+        "url": "https://itexamanswers.net/wp-content/uploads/2021/03/2021-03-20_155203.jpg",
+        "width": 1240,
+        "height": 827
+    },
+    "datePublished": "2019-12-20T15:40:28+0000",
+    "dateModified": "2024-10-07T10:33:07+0000",
+    "author": {
+        "@type": "Person",
+        "name": "IT Administrator"
+    },
+    "publisher": {
+        "@type": "Organization",
+        "name": "ITExamAnswers.net",
+        "logo": {
+            "@type": "ImageObject",
+            "url": "https://itexamanswers.net/wp-content/uploads/2019/02/itexam-1.png",
+            "width": 294,
+            "height": 70
+        }
+    },
+    "description": "[alert-success]If your exam ID does not match the IDs below, simply select an ID whose Network Address / LAN 1"
+}
+
+
+
+            function wpdm_rest_url(request) {
+                return "https://itexamanswers.net/wp-json/wpdm/" + request;
+            }
+        
+
+document.getElementById( "ak_js_1" ).setAttribute( "value", ( new Date() ).getTime() );
+
+document.getElementById( "ak_js_2" ).setAttribute( "value", ( new Date() ).getTime() );
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Q6162Q2XPK');
+
+
+
+{"prefetch":[{"source":"document","where":{"and":[{"href_matches":"\/*"},{"not":{"href_matches":["\/wp-*.php","\/wp-admin\/*","\/wp-content\/uploads\/*","\/wp-content\/*","\/wp-content\/plugins\/*","\/wp-content\/themes\/mts_splash\/*","\/*\\?(.+)"]}},{"not":{"selector_matches":"a[rel~=\"nofollow\"]"}},{"not":{"selector_matches":".no-prefetch, .no-prefetch a"}}]},"eagerness":"conservative"}]}
+
+
+
+                jQuery(function($){
+
+                    
+                                    });
+            
+
+
+/* <![CDATA[ */
+var mts_customscript = {"responsive":"1","nav_menu":"both"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var mts_ajax_search = {"url":"https:\/\/itexamanswers.net\/wp-admin\/admin-ajax.php","ajax_search":"1"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+function heateorSssLoadEvent(e) {var t=window.onload;if (typeof window.onload!="function") {window.onload=e}else{window.onload=function() {t();e()}}};	var heateorSssSharingAjaxUrl = 'https://itexamanswers.net/wp-admin/admin-ajax.php', heateorSssCloseIconPath = 'https://itexamanswers.net/wp-content/plugins/sassy-social-share/public/../images/close.png', heateorSssPluginIconPath = 'https://itexamanswers.net/wp-content/plugins/sassy-social-share/public/../images/logo.png', heateorSssHorizontalSharingCountEnable = 0, heateorSssVerticalSharingCountEnable = 0, heateorSssSharingOffset = -10; var heateorSssMobileStickySharingEnabled = 0;var heateorSssCopyLinkMessage = "Link copied.";var heateorSssUrlCountFetched = [], heateorSssSharesText = 'Shares', heateorSssShareText = 'Share';function heateorSssPopup(e) {window.open(e,"popUpWindow","height=400,width=600,left=400,top=100,resizable,scrollbars,toolbar=0,personalbar=0,menubar=no,location=no,directories=no,status")}
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var wpdiscuzAjaxObj = {"wc_hide_replies_text":"Hide Replies","wc_show_replies_text":"View Replies","wc_msg_required_fields":"Please fill out required fields","wc_invalid_field":"Some of field value is invalid","wc_error_empty_text":"please fill out this field to comment","wc_error_url_text":"url is invalid","wc_error_email_text":"email address is invalid","wc_invalid_captcha":"Invalid Captcha Code","wc_login_to_vote":"You Must Be Logged In To Vote","wc_deny_voting_from_same_ip":"You are not allowed to vote for this comment","wc_self_vote":"You cannot vote for your comment","wc_vote_only_one_time":"You've already voted for this comment","wc_voting_error":"Voting Error","wc_banned_user":"You are banned","wc_comment_edit_not_possible":"Sorry, this comment is no longer possible to edit","wc_comment_not_updated":"Sorry, the comment was not updated","wc_comment_not_edited":"You've not made any changes","wc_msg_input_min_length":"Input is too short","wc_msg_input_max_length":"Input is too long","wc_spoiler_title":"Spoiler Title","wc_cannot_rate_again":"You cannot rate again","wc_not_allowed_to_rate":"You're not allowed to rate here","wc_confirm_rate_edit":"Are you sure you want to edit your rate?","wc_follow_user":"Follow this user","wc_unfollow_user":"Unfollow this user","wc_follow_success":"You started following this comment author","wc_follow_canceled":"You stopped following this comment author.","wc_follow_email_confirm":"Please check your email and confirm the user following request.","wc_follow_email_confirm_fail":"Sorry, we couldn't send confirmation email.","wc_follow_login_to_follow":"Please login to follow users.","wc_follow_impossible":"We are sorry, but you can't follow this user.","wc_follow_not_added":"Following failed. Please try again later.","is_user_logged_in":"","commentListLoadType":"0","commentListUpdateType":"0","commentListUpdateTimer":"60","liveUpdateGuests":"0","wordpressThreadCommentsDepth":"5","wordpressIsPaginate":"","commentTextMaxLength":"0","replyTextMaxLength":"0","commentTextMinLength":"1","replyTextMinLength":"1","storeCommenterData":"100000","socialLoginAgreementCheckbox":"0","enableFbLogin":"0","fbUseOAuth2":"0","enableFbShare":"0","facebookAppID":"","facebookUseOAuth2":"0","enableGoogleLogin":"0","googleClientID":"","googleClientSecret":"","cookiehash":"92f2483bb651eed307c93e619d752c78","isLoadOnlyParentComments":"0","scrollToComment":"1","commentFormView":"collapsed","enableDropAnimation":"1","isNativeAjaxEnabled":"1","userInteractionCheck":"1","enableBubble":"0","bubbleLiveUpdate":"0","bubbleHintTimeout":"60","bubbleHintHideTimeout":"6","cookieHideBubbleHint":"wpdiscuz_hide_bubble_hint","bubbleHintShowOnce":"1","bubbleHintCookieExpires":"7","bubbleShowNewCommentMessage":"1","bubbleLocation":"content_left","firstLoadWithAjax":"0","wc_copied_to_clipboard":"Copied to clipboard!","inlineFeedbackAttractionType":"blink","loadRichEditor":"1","wpDiscuzReCaptchaSK":"","wpDiscuzReCaptchaTheme":"light","wpDiscuzReCaptchaVersion":"2.0","wc_captcha_show_for_guest":"0","wc_captcha_show_for_members":"0","wpDiscuzIsShowOnSubscribeForm":"0","wmuEnabled":"1","wmuInput":"wmu_files","wmuMaxFileCount":"1","wmuMaxFileSize":"2097152","wmuPostMaxSize":"104857600","wmuIsLightbox":"1","wmuMimeTypes":{"jpg":"image\/jpeg","jpeg":"image\/jpeg","jpe":"image\/jpeg","gif":"image\/gif","png":"image\/png","bmp":"image\/bmp","tiff":"image\/tiff","tif":"image\/tiff","ico":"image\/x-icon"},"wmuPhraseConfirmDelete":"Are you sure you want to delete this attachment?","wmuPhraseNotAllowedFile":"Not allowed file type","wmuPhraseMaxFileCount":"Maximum number of uploaded files is 1","wmuPhraseMaxFileSize":"Maximum upload file size is 2MB","wmuPhrasePostMaxSize":"Maximum post size is 100MB","wmuPhraseDoingUpload":"Uploading in progress! Please wait.","msgEmptyFile":"File is empty. Please upload something more substantial. This error could also be caused by uploads being disabled in your php.ini or by post_max_size being defined as smaller than upload_max_filesize in php.ini.","msgPostIdNotExists":"Post ID not exists","msgUploadingNotAllowed":"Sorry, uploading not allowed for this post","msgPermissionDenied":"You do not have sufficient permissions to perform this action","wmuKeyImages":"images","wmuSingleImageWidth":"auto","wmuSingleImageHeight":"200","previewTemplate":"<div class=\"wmu-preview [PREVIEW_TYPE_CLASS]\" title=\"[PREVIEW_TITLE]\" data-wmu-type=\"[PREVIEW_TYPE]\" data-wmu-attachment=\"[PREVIEW_ID]\">\r\n    <div class=\"wmu-preview-remove\">\r\n        <img class=\"wmu-preview-img\" src=\"[PREVIEW_ICON]\">\r\n        <div class=\"wmu-file-name\">[PREVIEW_FILENAME]<\/div>\r\n<!--        <div class=\"wmu-delete\">\u00a0<\/div>-->\r\n    <\/div>\r\n<\/div>\r\n","isUserRated":"0","version":"7.6.30","wc_post_id":"65237","isCookiesEnabled":"1","loadLastCommentId":"0","dataFilterCallbacks":[],"phraseFilters":[],"scrollSize":"32","url":"https:\/\/itexamanswers.net\/wp-admin\/admin-ajax.php","customAjaxUrl":"https:\/\/itexamanswers.net\/wp-content\/plugins\/wpdiscuz\/utils\/ajax\/wpdiscuz-ajax.php","bubbleUpdateUrl":"https:\/\/itexamanswers.net\/wp-json\/wpdiscuz\/v1\/update","restNonce":"bc8e1a5c90","is_rate_editable":"0","menu_icon":"https:\/\/itexamanswers.net\/wp-content\/plugins\/wpdiscuz\/assets\/img\/plugin-icon\/wpdiscuz-svg.svg","menu_icon_hover":"https:\/\/itexamanswers.net\/wp-content\/plugins\/wpdiscuz\/assets\/img\/plugin-icon\/wpdiscuz-svg_hover.svg","is_email_field_required":"1"};
+var wpdiscuzUCObj = {"msgConfirmDeleteComment":"Are you sure you want to delete this comment?","msgConfirmCancelSubscription":"Are you sure you want to cancel this subscription?","msgConfirmCancelFollow":"Are you sure you want to cancel this follow?","additionalTab":"0"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+        var wpdiscuzEditorOptions = {
+        modules: {
+        toolbar: "",
+        counter: {
+        uniqueID: "",
+        commentmaxcount : 0,
+        replymaxcount : 0,
+        commentmincount : 1,
+        replymincount : 1,
+        },
+                },
+        wc_be_the_first_text: "Be the First to Comment!",
+        wc_comment_join_text: "Join the discussion",
+        theme: 'snow',
+        debug: 'error'
+        };
+        
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var IvorySearchVars = {"is_analytics_enabled":"1"};
+/* ]]> */
+
+
+
+/* <![CDATA[ */
+var flra_array = {"MainContentW":"1140","LeftBannerW":"118","RightBannerW":"118","LeftAdjust":"30","RightAdjust":"10","TopAdjust":"30","TopAdjustScroll":"0"};
+/* ]]> */
+
+
+!function(t){"function"==typeof define&&define.amd?define(t):t()}(function(){var t;!function(){var t=[],e=["click","mousemove","keydown","touchstart","touchmove","wheel"],n=document.querySelectorAll("script[data-src]"),r=document.querySelectorAll("link[data-href]");if(n.length||r.length){var a=function(e){return t.push(e)};document.addEventListener("click",a,{passive:!0});var o=setTimeout(c,10000);e.forEach(function(t){window.addEventListener(t,c,{passive:!0})})}function i(e){var r=n[e];r.onload=function(){if(e>=n.length-1)return window.dispatchEvent(new Event("DOMContentLoaded")),window.dispatchEvent(new Event("load")),document.removeEventListener("click",a),void t.forEach(function(t){var e=new MouseEvent("click",{view:t.view,bubbles:!0,cancelable:!0});t.target.dispatchEvent(e)});i(e+1)},r.src=r.getAttribute("data-src")}function c(){clearTimeout(o),e.forEach(function(t){window.removeEventListener(t,c,{passive:!0})}),n.forEach(function(t){var e=t.getAttribute("data-src");if(!e.startsWith("data:")){var n=document.createElement("link");n.rel="preload",n.as="script",n.href=e,document.head.appendChild(n)}}),n.length&&i(0),r.forEach(function(t){t.href=t.getAttribute("data-href")})}}(),t=new IntersectionObserver(function(e){e.forEach(function(e){e.isIntersecting&&(t.unobserve(e.target),e.target.getAttribute("data-lazy-attributes").split(",").forEach(function(t){var n=e.target.getAttribute("data-lazy-".concat(t));e.target.setAttribute(t,n)}))})},{rootMargin:"300px"}),document.querySelectorAll("[data-lazy-method='viewport']").forEach(function(e){t.observe(e)})});
